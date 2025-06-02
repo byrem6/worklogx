03c51c0df710a670fc61035db728772d87af1321
Ramazan Hocaoglu <remindfunction@gmail.com>
2025-06-02 20:34:42 +0300

    fdx

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,17 +27,17 @@ const RESOURCES = {"assets/AssetManifest.bin": "7fc04c47a7a61c6c8a2c7caa2820aa0a
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "8953671643fd1c072b21a3eee1edf61c",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6b41fcc440d4f07578bc44b31128496e",
+"flutter_bootstrap.js": "17f5f6508e134400e827aa69a0282d9c",
 "icons/Icon-192.png": "77276a3fabf7bd0ede9ceaf44f6930ed",
 "icons/Icon-512.png": "f949bb3cc4ae8d084b5ba903116ddfbd",
 "icons/Icon-maskable-192.png": "77276a3fabf7bd0ede9ceaf44f6930ed",
 "icons/Icon-maskable-512.png": "f949bb3cc4ae8d084b5ba903116ddfbd",
-"index.html": "06ee0e405bbec1b140b0dd17f22847b5",
-"/": "06ee0e405bbec1b140b0dd17f22847b5",
+"index.html": "6b3d15d337c9055482eed3384d60ac29",
+"/": "6b3d15d337c9055482eed3384d60ac29",
 "logo.png": "a6d21e1cb1b91c2644c3e0f8500059e6",
-"main.dart.js": "ef560c981a0951ad6679c0dda574f6b3",
+"main.dart.js": "242ca115aeafb862110b7b1ddfdd015d",
 "manifest.json": "62ef744ec2fe666167a3724afab173e8",
-"version.json": "f14f1b1052f7890542e9e828d3c5ab76"};
+"version.json": "7e7b0f0e8a343204cae1d559c6119852"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
