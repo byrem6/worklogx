c23aab4e57709762b443348d1f28a2e670de5d76
Ramazan Hocaoglu <remindfunction@gmail.com>
2025-12-31 12:19:21 +0300

    adadaa333

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3880622157"
+    serviceWorkerVersion: "4213684260"
   }
 });
